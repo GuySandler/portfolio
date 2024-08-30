4ee90d2a81d39698406cc0f09742a2af197bfa06
Guy Sandler <guy@sandler.co.il>
2024-08-30 16:48:16 -0700

    restarted 3
    sidebar css

--- src/routes/sidebar.svelte
+++ src/routes/sidebar.svelte
@@ -12,18 +12,18 @@
     let MouseX = 0;
     let MouseY = 0;
     function handleMouseMove(e) {
-        MouseX = e.clientX-350;
-        MouseY = e.clientY-350;
+        MouseX = e.clientX-500;
+        MouseY = e.clientY-250;
     }
-    $: Geffectx = MouseX/50;
-    $: Geffecty = MouseY/50;
+    $: Geffectx = MouseX/100;
+    $: Geffecty = MouseY/75;
 </script>
 
 <svelte:window on:mousemove={handleMouseMove}/>
 
 <main>
     <center>
-        <div id="container">
+        <div id="container" class="md:flex sh:overflow-y-auto">
             <div class="item order-1">
                 <div id="logoBox">
                     <img style="fill:white;z-index: 1" alt="G" src={G1} />
@@ -35,11 +35,21 @@
             </div>
             <div class="item order-3">
                 <div id="menu">
-                    <button><img class="image" alt="home" src={house} /></button>
-                    <button><img class="image" alt="projects" src={cube} /></button>
-                    <button><img class="image" alt="skills" src={gear} /></button>
-                    <button><img class="image" alt="info" src={info} /></button>
-                    <button><img class="image" alt="awards" src={awards} /></button>
+                    <button class="extraPad">
+                        <img class="image" alt="home" src={house} />
+                    </button>
+                    <button class="extraPad">
+                        <button><img class="image" alt="projects" src={cube} /></button>
+                    </button>
+                    <button class="extraPad">
+                        <button><img class="image" alt="skills" src={gear} /></button>
+                    </button>
+                    <button class="extraPad">
+                        <button><img class="image" alt="info" src={info} /></button>
+                    </button>
+                    <button class="extraPad">
+                        <button><img class="image" alt="awards" src={awards} /></button>
+                    </button>
                 </div>
             </div>
             <div class="item order-4">
@@ -47,8 +57,12 @@
             </div>
             <div class="item order-5">
                 <div id="contact">
-                    <button><img id="git" style="width:7vh;height:7vh;margin-bottom: 5px;margin-top: 30px" alt="github" src={github} /></button>
-                    <button><img class="image" alt="email" src={letter} /></button>
+                    <button class="extraPad">
+                        <img id="git" alt="github" src={github} />
+                    </button>
+                    <button class="extraPad">
+                        <img class="image" alt="email" src={letter} />
+                    </button>
                 </div>
             </div>
         </div>
@@ -62,49 +76,73 @@
         /*width: 10%;*/
         width: 100px;
         height: 99.9vh;
-        justify-content: center;
         flex-flow: row wrap;
         overflow-y: scroll;
-        /*overflow-x: hidden;*/
         background-color: #121212;
         overflow-x: hidden;
     }
     .item {
         width: 100px;
         /*border: purple 1px solid;*/
-        margin-right: auto;
     }
     #spacer {
         height: 5vh;
+        /*border: purple solid 1px;*/
     }
+
     .image {
-        width:7vh;
-        height:7vh;
-        margin-bottom: 5px;
-        margin-top: 5px;
-        min-height: 100px;
+        width:6vh;
+        height:6vh;
+        /*margin-bottom: 5px;*/
+        /*margin-top: 5px;*/
+        /*min-height: 100px;*/
         /*margin-left: 7px;*/
+        /*border: white 1px solid;*/
+    }
+    #git {
+        width:6vh;
+        height:6vh;
+        /*border: white 1px solid;*/
+        /*margin-bottom: 10px;*/
+        /*margin-top: 30px*/
     }
     @keyframes buttonHover {
-         from{transform: translate(0, 0px);}
-         to{transform: translate(0, -10px);}
-     }
-    .image:hover, #git:hover {
+        from {
+            filter: drop-shadow(0 0 0em #ffffff);
+        }
+        to {
+            filter: drop-shadow(0 0 5em #ffffff);
+        }
+    }
+    .extraPad:active {
         filter: drop-shadow(0 0 2em #747bff);
-        /*transform: translate(0, -10px);*/
-        animation: 3s ease-in-out 1s 2 reverse both paused buttonHover;
+        animation: .25s forwards ease-in-out buttonHover;
+    /*    fix animation early stop*/
     }
     #menu {
-        margin-bottom: 10px;
-        margin-top: 10px;
+        margin-bottom: 5px;
+        margin-top: 5px;
+    }
+    .extraPad {
+        /*border: red solid 1px;*/
+        display: flex;
+        width: 100%;
+        justify-content: center;
+        flex-flow: row wrap;
+        overflow-y: scroll;
+        background-color: #121212;
+        overflow-x: hidden;
+        margin-top: 5vh;
+        margin-bottom: 5vh;
+        border-radius: 100px;
     }
     ::-webkit-scrollbar {
         width: 5px;
 
     }
-    ::-webkit-scrollbar-track {
-        background: #404040;
-    }
+    /*::-webkit-scrollbar-track {*/
+    /*    background: #404040;*/
+    /*}*/
     ::-webkit-scrollbar-thumb {
         background: #888;
     }
